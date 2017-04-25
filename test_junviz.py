a936accaec61992e7eff42091adf2ff06fe9000c
Mikael Johansson <mik.json@gmail.com>
2017-04-25 20:11:32 +0200

    Update test_junviz.py

--- test_junviz.py
+++ test_junviz.py
@@ -8,11 +8,11 @@ direction 	= sys.argv[2]
 
 viz = jViz('juniperconf_xml_2012_07_26.txt',verbose=True)
 
-#viz.parseconf('<policies>','</policies>')
+viz.parse_conf('<policies>','</policies>')
 policies, zones = viz.getpolicies()
-viz.creategraph(zone,direction,gdir='/root/bin/gviz/')
+viz.create_graph(zone,direction,gdir='/root/bin/gviz/')
 
-#viz.getfwinfo()
-viz.closeconf()
+viz.get_fwinfo()
+viz.close_conf()
 
 print 'Done!'
